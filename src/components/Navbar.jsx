a29b7c84cc8053493003bbed6948e777d9e03b0b
wobbethan <wobbethan@gmail.com>
2023-03-26 22:13:00 -0400

    final commit before optimization

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,8 @@ import { Link } from 'react-router-dom';
 
 import { styles } from '../styles';
 import { navLinks } from '../constants';
-import { logo,menu,close } from '../assets';
+import { menu,close } from '../assets';
+import ewlogo from '../assets/ewlogo.png'
 
 const Navbar = () => {
   const [active, setActive] = useState("");
@@ -24,7 +25,7 @@ const Navbar = () => {
             window.scrollTo(0, 0);
         }}
         >
-          <img src={logo} alt="logo" className = "w-9 h-9 object-contain" />
+          <img src={ewlogo} alt="logo" className = "w-9 h-9 object-contain" />
           <p className = "text-white text-[18px] font-bold cursor-pointer flex">
             Ethan Wobb &nbsp;
             <span className="sm:block hidden"> | Computer Science Undergrad</span>
